f86292f3469ee4d34e061598236f4aeb07387e2a
Jaime <jriverarojas@gmail.com>
2024-07-26 19:39:24 -0400

    fixes

--- src/messenger/controller/queue.controller.ts
+++ src/messenger/controller/queue.controller.ts
@@ -17,7 +17,6 @@ export class QueueController {
     @Body() body: any
   ): Promise<{ id: string }> {
     try {
-      console.log('enque', instance, channel,type, body )
       const id = await this.redisService.addToQueue({
         ...body,
         type,
