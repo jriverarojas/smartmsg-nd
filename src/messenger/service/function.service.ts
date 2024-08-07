f410ba6bcc2966acfdf19406cb64c51424fd110d
Jaime <jriverarojas@gmail.com>
2024-08-07 09:17:24 -0400

    fixes

--- src/messenger/service/function.service.ts
+++ src/messenger/service/function.service.ts
@@ -8,14 +8,57 @@ import { FunctionCall } from '../entities/functioncall.entity';
 import { EncryptionService } from 'src/auth/service/encryption.service';
 import { Thread } from '../entities/thread.entity';
 import { AutomaticService } from './automatic.service';
-
+import { CreateFunctionDto } from '../dto/create-function.dto';
+import { UpdateFunctionDto } from '../dto/update-function.dto';
 @Injectable()
 export class FunctionService {
   constructor(
     private readonly automaticService: AutomaticService,
     private readonly dataSource: DataSource,
     private readonly encryptionService: EncryptionService,
+    @InjectRepository(Function)
+    private functionRepository: Repository<Function>,
   ) {}
+
+  async create(createFunctionDto: CreateFunctionDto): Promise<Function> {
+    const { url, headers, ...rest } = createFunctionDto;
+    const encryptedUrl = this.encryptionService.encrypt(url);
+    const encryptedHeaders = this.encryptionService.encrypt(headers);
+
+    const func = this.functionRepository.create({
+      ...rest,
+      url: encryptedUrl,
+      headers: encryptedHeaders,
+    });
+
+    return this.functionRepository.save(func);
+  }
+
+  async findAll(): Promise<Function[]> {
+    return this.functionRepository.find();
+  }
+
+  async findOne(id: number): Promise<Function> {
+    return this.functionRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateFunctionDto: UpdateFunctionDto): Promise<Function> {
+    const { url, headers, ...rest } = updateFunctionDto;
+    const encryptedUrl = this.encryptionService.encrypt(url);
+    const encryptedHeaders = this.encryptionService.encrypt(headers);
+
+    await this.functionRepository.update(id, {
+      ...rest,
+      url: encryptedUrl,
+      headers: encryptedHeaders,
+    });
+
+    return this.functionRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.functionRepository.delete(id);
+  }
 
   async execute(functionEntities: Function, queueItem: any) {
 
@@ -24,8 +67,6 @@ export class FunctionService {
     await queryRunner.connect();
     await queryRunner.startTransaction();
     const { functions, threadId, instance, channel, assistantConfig, origin, firedBy, runId } = queueItem;
-
-    let finalResponse = '';
 
     try {
       let index = 0;
